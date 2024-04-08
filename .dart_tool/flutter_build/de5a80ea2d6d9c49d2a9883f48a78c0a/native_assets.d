f805a90f58589e94739ac821e338c96a1e838c0a
Ayan-Shahid <63579232+Ayan-Shahid@users.noreply.github.com>
2024-04-08 19:47:45 +0500

    Inital

---
+++ .dart_tool/flutter_build/de5a80ea2d6d9c49d2a9883f48a78c0a/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/Ayan/Flutter\ Apps/ChatZen-Chat-Bot/.dart_tool/flutter_build/de5a80ea2d6d9c49d2a9883f48a78c0a/native_assets.yaml: 
---
+++ android/app/src/main/kotlin/com/example/chatzen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatzen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
